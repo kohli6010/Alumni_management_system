dce0e8f2249a7657d7035160d6ecf7e51e8802f7
Rahul <kohli6010@gmail.com>
2020-01-25 11:37:23 +0530

    Added login popup functionality

--- public/script.js
+++ public/script.js
@@ -2,6 +2,9 @@ const collegeListBtn = document.getElementById('college');
 const collegeList = document.getElementById("colleges");
 const aboutBtn = document.getElementById("about");
 const aboutUs = document.getElementById("about-us");
+const loginBtn = document.getElementById("login");
+const loginPopUp = document.getElementsByClassName("login-popup");
+const cross = document.querySelector(".cancel>img");
 
 collegeListBtn.addEventListener('click', () => {
 	collegeList.scrollIntoView();
@@ -9,4 +12,12 @@ collegeListBtn.addEventListener('click', () => {
 
 aboutBtn.addEventListener('click', () => { 
 	aboutUs.scrollIntoView();
+})
+
+loginBtn.addEventListener('click', () => {
+	loginPopUp[0].classList.add("login-pop-animation")
+})
+
+cross.addEventListener("click", () => { 
+	loginPopUp[0].classList.remove("login-pop-animation");
 })
