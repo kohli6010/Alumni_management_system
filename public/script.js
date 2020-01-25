ec6c46c1571c70753dbc8ceb371c00a519800610
Rahul <kohli6010@gmail.com>
2020-01-25 15:28:38 +0530

    Added Slight changes to the page

--- public/script.js
+++ public/script.js
@@ -5,6 +5,8 @@ const aboutUs = document.getElementById("about-us");
 const loginBtn = document.getElementById("login");
 const loginPopUp = document.getElementsByClassName("login-popup");
 const cross = document.querySelector(".cancel>img");
+const nav = document.querySelector("nav");
+const main = document.querySelector("main");
 
 collegeListBtn.addEventListener('click', () => {
 	collegeList.scrollIntoView();
@@ -15,9 +17,18 @@ aboutBtn.addEventListener('click', () => {
 })
 
 loginBtn.addEventListener('click', () => {
-	loginPopUp[0].classList.add("login-pop-animation")
+	loginPopUp[0].classList.add("login-pop-animation");
+	main.style.filter = 'grayscale(100%)';
+	nav.style.filter = "grayscale(100%)";
+	collegeList.style.filter = "grayscale(100%)";
+	aboutUs.style.filter = "grayscale(100%)"
+
 })
 
 cross.addEventListener("click", () => { 
 	loginPopUp[0].classList.remove("login-pop-animation");
+	main.style.filter = 'grayscale(0%)';
+	nav.style.filter = "grayscale(0%)";
+	collegeList.style.filter = "grayscale(0%)";
+	aboutUs.style.filter = "grayscale(0%)"
 })
