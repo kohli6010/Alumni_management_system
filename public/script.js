e13406be971a811f4ade82e12240e47991770682
Rahul <kohli6010@gmail.com>
2020-02-05 08:59:17 +0530

    Added Script and styles

--- public/script.js
+++ public/script.js
@@ -1,34 +1,33 @@
 const collegeListBtn = document.getElementById('college');
-const collegeList = document.getElementById("colleges");
-const aboutBtn = document.getElementById("about");
-const aboutUs = document.getElementById("about-us");
-const loginBtn = document.getElementById("login");
-const loginPopUp = document.getElementsByClassName("login-popup");
-const cross = document.querySelector(".cancel>img");
-const nav = document.querySelector("nav");
-const main = document.querySelector("main");
+const collegeList = document.getElementById('colleges');
+const aboutBtn = document.getElementById('about');
+const aboutUs = document.getElementById('about-us');
+const loginBtn = document.getElementById('login');
+const loginPopUp = document.getElementsByClassName('login-popup');
+const cross = document.querySelector('.cancel>img');
+const nav = document.querySelector('nav');
+const main = document.querySelector('main');
 
 collegeListBtn.addEventListener('click', () => {
-	collegeList.scrollIntoView();
+    collegeList.scrollIntoView();
 });
 
-aboutBtn.addEventListener('click', () => { 
-	aboutUs.scrollIntoView();
-})
+aboutBtn.addEventListener('click', () => {
+    aboutUs.scrollIntoView();
+});
 
 loginBtn.addEventListener('click', () => {
-	loginPopUp[0].classList.add("login-pop-animation");
-	main.style.filter = 'grayscale(100%)';
-	nav.style.filter = "grayscale(100%)";
-	collegeList.style.filter = "grayscale(100%)";
-	aboutUs.style.filter = "grayscale(100%)"
+    loginPopUp[0].classList.add('login-pop-add-animation');
+    main.style.filter = 'grayscale(100%)';
+    nav.style.filter = 'grayscale(100%)';
+    collegeList.style.filter = 'grayscale(100%)';
+    aboutUs.style.filter = 'grayscale(100%)';
+});
 
-})
-
-cross.addEventListener("click", () => { 
-	loginPopUp[0].classList.remove("login-pop-animation");
-	main.style.filter = 'grayscale(0%)';
-	nav.style.filter = "grayscale(0%)";
-	collegeList.style.filter = "grayscale(0%)";
-	aboutUs.style.filter = "grayscale(0%)"
-})
+cross.addEventListener('click', () => {
+    loginPopUp[0].classList.remove('login-pop-add-animation');
+    main.style.filter = 'grayscale(0%)';
+    nav.style.filter = 'grayscale(0%)';
+    collegeList.style.filter = 'grayscale(0%)';
+    aboutUs.style.filter = 'grayscale(0%)';
+});
