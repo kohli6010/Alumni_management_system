32ec5d6f4a175989024e8078446d6031730fb355
Rahul <kohli6010@gmail.com>
2020-02-05 21:27:04 +0530

    Added Popup not whole functionality

---
+++ public/login_page/loginpage.js
@@ -0,0 +1,13 @@
+let events = document.querySelectorAll(".events");
+let flipcard = document.querySelector(".flipcard");
+let cancel = document.querySelector(".cancel");
+
+for (let i = 0; i < events.length; i++) { 
+	events[i].addEventListener("click", () => { 
+		flipcard.style.display = "block";
+	})
+}
+
+cancel.addEventListener("click", () => { 
+	flipcard.style.display = "none";
+})
