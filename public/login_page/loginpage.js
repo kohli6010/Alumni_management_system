9bf429018f4f05eca0b4cc7fd952ffe5e1625375
Rahul <kohli6010@gmail.com>
2020-02-06 13:50:12 +0530

    Added Logout fucnctionality

--- public/login_page/loginpage.js
+++ public/login_page/loginpage.js
@@ -2,6 +2,7 @@ let events = document.querySelectorAll(".events");
 let flipcard = document.querySelector(".flipcard");
 let cancel = document.querySelector(".cancel");
 
+
 for (let i = 0; i < events.length; i++) { 
 	events[i].addEventListener("click", () => { 
 		flipcard.style.display = "block";
